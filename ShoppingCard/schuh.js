5cf7bd80bbe542608a1d2687b12b541405c8c3d7
BabSchu <you@example.com>
2019-10-08 10:24:04 +0200

    add file basic 2 and folder shopping card

---
+++ ShoppingCard/schuh.js
@@ -0,0 +1,160 @@
+var shoes = [{
+
+	"id": "01",
+
+	"Typ": "Sneaker ",
+
+	"Marke": "Adidas",
+
+	"Modell": "Terminator Predator 2 ",
+
+	"Img_Link": "img/01.jpg",
+
+	"Preis": 50,
+
+
+   }, {
+
+	"id": "02",
+
+	"Typ": "Flip-Flop ",
+
+	"Marke": "Balenciaga",
+
+	"Modell": "The coolest flipflop ",
+
+	"Img_Link": "img/02.jpg",
+	
+	"Preis": 5000
+
+	}, {
+
+	"id": "03",
+
+	"Typ": "High Heels shoes ",
+
+	"Marke": "High",
+
+	"Modell": "Nice High Heels",
+
+	"Img_Link": "img/03.jpg",
+	
+	"Preis": 500
+
+	}, {
+
+	"id": "04",
+
+	"Typ": "Brogue ",
+
+	"Marke": "Church's",
+
+	"Modell": "Classic",
+
+	"Img_Link": "img/04.jpg",
+	
+	"Preis": 300
+
+	}, {
+
+	"id": "05",
+
+	"Typ": "Sandals ",
+
+	"Marke": "Nike",
+
+	"Modell": "Overpriced Plastic Sandals  ",
+
+	"Img_Link": "img/05.jpg",
+	
+	"Preis": 50
+
+	}, {
+
+	"id": "06",
+
+	"Typ": "Running Shoes ",
+
+	"Marke": "Mizuno",
+
+	"Modell": "Wave 2019",
+
+	"Img_Link": "img/06.jpg",
+	
+	"Preis": 150
+
+	}, {
+
+	"id": "07",
+
+	"Typ": "Leather Shoes ",
+
+	"Marke": "D'Acquasparta",
+
+	"Modell": "Everyday",
+
+	"Img_Link": "img/07.jpg",
+	
+	"Preis": 200
+
+	}, {
+
+	"id": "08",
+
+	"Typ": "Sneaker ",
+
+	"Marke": "Converse",
+
+	"Modell": "Athletics Bio",
+
+	"Img_Link": "img/08.jpg",
+	
+	"Preis": 180
+
+	}, {
+
+	"id": "09",
+
+	"Typ": "Sneaker ",
+
+	"Marke": "Tommy Hilfiger",
+
+	"Modell": "Cool knowledgeable guy",
+
+	"Img_Link": "img/09.jpg",
+	
+	"Preis": 200
+
+	}, {
+
+	"id": "10",
+
+	"Typ": "Sneaker ",
+
+	"Marke": "Nike",
+
+	"Modell": "We are not exploiting kids to make this shoe",
+
+	"Img_Link": "img/10.jpg",
+	
+	"Preis": 100
+
+	}]
+
+
+/*var i = 0
+    document.writeln("<table border='2'><tr>");
+    for (i = 0; i < shoes.length; i++) {
+        document.writeln("<td>");
+        document.writeln("<table border='2' width=100 >");
+        document.writeln("<tr><td><b>id</b></td><td width=50>" + shoes[i].id +"</td></tr>");
+        document.writeln("<tr><td><b>Typ</b></td><td width=50>" + shoes[i].Typ + "</td></tr>");
+        document.writeln("<tr><td><b>Marke</b></td><td width=50>" + shoes[i].Marke + "</td></tr>");
+        document.writeln("<tr><td><b>Modell</b></td><td width=50>" + shoes[i].Modell + "</td></tr>");
+        document.writeln("<tr><td><b>Preis</b></td><td width=50>" + shoes[i].Img_Link + "</td></tr>");
+        document.writeln("<tr><td><b>Preis</b></td><td width=50>" + shoes[i].Preis + "</td></tr>");
+        document.writeln("</table>");
+        document.writeln("</td>");
+    }*/
+
+document.getElementById("shoe1").innerHTML = "shoes[0].id";
---
+++ ShoppingCard/shoppingcard.js
@@ -0,0 +1,16 @@
+/*for (var i = 0; i > document.querySelectorAll(".btn").length; i++ ) {
+			document.querySelectorAll(".btn")[i].addEventListener("click",)
+		}
+*/
+
+function del5() {
+	document.getElementById('shoe5').style.display = 'none';
+}
+
+document.getElementById("btn5").onclick = del5();
+
+function del6() {
+	document.getElementById('shoe6').style.display = 'none';
+}
+
+document.getElementById("btn6").onclick = del6();
